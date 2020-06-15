d724c8e09d5b387db8b7a3a3c3a092ff4f6b55bd
Byron Lathi <byron.lathi@student.lovett.org>
2020-06-14 21:57:20 -0500

    Change screen ram to be full dual-ported instead of read/write. Allow computer to read data from screen memory.

--- fpga_graphics_adapter.v
+++ fpga_graphics_adapter.v
@@ -27,6 +27,7 @@ module fpga_graphics_adapter (
 	
 	
 	(*keep*) wire [15:0] screen_r_address;
+	(*keep*) wire [15:0] draw_screen_r_address;
 	(*keep*) wire [15:0] screen_w_address;
 	
 	(*keep*) wire [7:0] chr_sub;
@@ -42,7 +43,7 @@ module fpga_graphics_adapter (
 	assign chipclk = clk_ext1 & ~cs;
 	
 	wire [7:0] data_in;
-	wire [7:0] data_out;
+	(*keep*)wire [7:0] data_out;
 	
 	assign data_bi = (wren & chipclk) ? data_out : 8'bZ;
 	assign data_in = data_bi;
@@ -75,12 +76,13 @@ module fpga_graphics_adapter (
 	
 	
 	screen_ram d  (
-		.rdaddress (screen_r_address),
-		.wraddress (screen_w_address),
+		.address_a (screen_r_address),
+		.address_b (screen_w_address),
 		.clock (fclock),
-		.data (int_reg[1]),
-		.wren (wren_screen),
-		.q (screen_data)
+		.data_b (int_reg[1]),
+		.wren_b (wren_screen),
+		.q_a (screen_data),
+		.q_b (data_out)
 	);
 	
 	/*
@@ -161,8 +163,12 @@ module fpga_graphics_adapter (
 		curr_addr = rs;
 	end
 	
-	always @ (posedge (~wren & ~cs & clk)) begin		// Main code should run here, after data has been recieved
-		int_reg[curr_addr] = data_in;
+	always @ (posedge clk) begin		// Main code should run here, after data has been recieved
+		if (~cs) begin
+			if (~wren) begin
+				int_reg[curr_addr] = data_in;		// Write data to register
+			end
+		end
 	end
 
 	always @(posedge vga_clk) begin
